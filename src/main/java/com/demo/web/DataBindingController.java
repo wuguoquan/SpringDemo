bf34bd346c20487e1c59eb3f09f1708a5a2910db
wuguoquan <zjuwuguoquan@gmail.com>
2017-09-15 19:24:48 -0700

    [Web] SpringDemo: @PathVariable @RequestHeader @CookieValue

---
+++ src/main/java/com/demo/web/DataBindingController.java
@@ -0,0 +1,45 @@
+package com.demo.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DataBindingController {
+
+    // 静态的日志类LogFactory
+    private static final Log logger = LogFactory
+            .getLog(DataBindingController.class);
+
+    // 测试@PathVariable注解
+    // 该方法映射的请求为http://localhost:8080/Spring/pathVariableTest/{userId}
+    @RequestMapping(value="/pathVariableTest/{userId}")
+    public void pathVariableTest(
+            @PathVariable Integer userId) {
+        logger.info("通过@PathVariable获得数据： " + userId);
+    }
+
+    // 测试@RequestHeader注解
+    // 该方法映射的请求为http://localhost:8080/Spring/requestHeaderTest
+    @RequestMapping(value="/requestHeaderTest")
+    public void requestHeaderTest(
+            @RequestHeader("User-Agent") String userAgent,
+            @RequestHeader(value="Accept") String[] accepts) {
+        logger.info("通过@requestHeaderTest获得数据： " + userAgent);
+        for(String accept : accepts){
+            logger.info(accept);
+        }
+    }
+
+    // 测试@CookieValue注解
+    @RequestMapping(value="/cookieValueTest")
+    public void cookieValueTest(
+            @CookieValue(value="JSESSIONID", defaultValue="") String sessionId) {
+        logger.info("通过@requestHeaderTest获得数据： " + sessionId);
+    }
+
+}
