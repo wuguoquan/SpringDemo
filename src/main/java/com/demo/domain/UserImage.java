edcb3f89ccd81781d02e70cf9862678f8a4b1ad1
wuguoquan <zjuwuguoquan@gmail.com>
2017-09-16 20:03:58 -0700

    [Web] SpringDemo: FileUploadTest

---
+++ src/main/java/com/demo/domain/UserImage.java
@@ -0,0 +1,38 @@
+package com.demo.domain;
+
+import java.io.Serializable;
+
+import org.springframework.web.multipart.MultipartFile;
+
+// 域对象，实现序列化接口
+public class UserImage implements Serializable{
+
+	private String username;
+	private MultipartFile image;
+
+	public UserImage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public MultipartFile getImage() {
+		return image;
+	}
+
+	public void setImage(MultipartFile image) {
+		this.image = image;
+	}
+
+	
+
+	
+	
+}
